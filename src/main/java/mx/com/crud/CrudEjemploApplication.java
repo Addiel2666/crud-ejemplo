4f5e7a6ad817b544ce2379c9fb1376df9fc9b16d
addiel.hernandez-at-342220745908 <addiel.hernandez@fygsolutions.com>
2025-01-05 10:56:30 -0600

    [UPDATE]Se sube codigo

---
+++ src/main/java/mx/com/crud/CrudEjemploApplication.java
@@ -0,0 +1,13 @@
+package mx.com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEjemploApplication.class, args);
+	}
+
+}
